6686e0563f72d4987576236a56d1540993ab7d72
HRS <pascal@heavyrubberslave.com>
2026-02-07 15:57:23 +0100

    Get rid of some heavy stuff

--- examples/basic/src/main.cpp
+++ examples/basic/src/main.cpp
@@ -4,27 +4,27 @@
 #include <SlvCtrlArduinoSerialCommandsTransport.h>
 
 // --------- your state ----------
-static int  speed    = 0;
-static int  maxSpeed = 100;
-static int  state    = 0;
+static int32_t  speed    = 0;
+static int32_t  maxSpeed = 100;
+static int32_t  state    = 0;
 static bool ready    = false;
 
 // --------- getters/setters (C function pointers) ----------
 // Note: your attribute classes use function pointers: T(*)(void*), not lambdas.
-static int getSpeed(void*) { return speed; }
-static SlvCtrlParseError setSpeed(void*, int v) { speed = v; return SlvCtrlParseError::Ok; }
+static int32_t getSpeed(void*) { return speed; }
+static SlvCtrlParseError setSpeed(void*, int32_t v) { speed = v; return SlvCtrlParseError::Ok; }
 
-static int getMaxSpeed(void*) { return maxSpeed; }
-static SlvCtrlParseError setMaxSpeed(void*, int v) { maxSpeed = v; return SlvCtrlParseError::Ok; }
+static int32_t getMaxSpeed(void*) { return maxSpeed; }
+static SlvCtrlParseError setMaxSpeed(void*, int32_t v) { maxSpeed = v; return SlvCtrlParseError::Ok; }
 
-static int getState(void*) { return state; }
-static SlvCtrlParseError setState(void*, int v) { state = v; return SlvCtrlParseError::Ok; }
+static int32_t getState(void*) { return state; }
+static SlvCtrlParseError setState(void*, int32_t v) { state = v; return SlvCtrlParseError::Ok; }
 
 static bool getReady(void*) { return ready; }
 
 // --------- attributes ----------
 static IntAttribute speedAttr("speed", &getSpeed, &setSpeed);
-static RangeAttribute<int> maxAttr("max", &getMaxSpeed, &setMaxSpeed, 0, 100);
+static RangeAttribute<int32_t> maxAttr("max", &getMaxSpeed, &setMaxSpeed, 0, 100);
 static IntAttribute stateAttr("state", &getState, &setState);
 static BoolAttribute readyAttr("ready", &getReady, nullptr);
 
